b1e7443f50c166696fdbcf8672df9a40898f07ab
Milco Numan <milco.numan@gmail.com>
2024-09-28 17:23:19 +0000

    1.4

--- pcost.py
+++ pcost.py
@@ -1,6 +1,16 @@
-with open("Data/portfolio.dat") as portfolio:
-    total = 0
-    for line in portfolio:
-        stock, qty, price = line.split()
-        total += int(qty) * float(price)
-print(f"Total portfolio cost {total}")
+def portfolio_cost(fname: str) -> float:
+    with open(fname) as portfolio:
+        total = 0
+        for line in portfolio:
+            fields = line.split()
+            try:
+                qty = int(fields[1])
+                price = float(fields[2])
+                total += qty * price
+            except ValueError as ve:
+                print(f"Couldn't parse {line}")
+                print(ve)
+    return total
+
+
+print(portfolio_cost("Data/portfolio3.dat"))
