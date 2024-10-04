619cd5763f31132c610bb8cc971abfceb17d7216
Milco Numan <milco.numan@gmail.com>
2024-10-04 11:08:02 +0000

    Playing around

---
+++ test_prop_access.py
@@ -0,0 +1,15 @@
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
