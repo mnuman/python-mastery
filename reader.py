ca9504f7cbabf05a99f4d83bc7e710928447835c
Milco Numan <milco.numan@gmail.com>
2024-10-04 10:52:36 +0000

    3.3

--- reader.py
+++ reader.py
@@ -10,3 +10,16 @@ def read_csv_as_dicts(filename, conversions):
                 conversion(value) for conversion, value in zip(conversions, row)
             ])) for row in reader
         ]
+
+
+def read_csv_as_instances(filename, cls):
+    '''
+    Read a CSV file into a list of instances
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            records.append(cls.from_row(row))
+    return records
--- stock.py
+++ stock.py
@@ -4,6 +4,7 @@ headers = []
 
 
 class Stock:
+    types = (str, int, float)
 
     def __init__(self, name, shares, price):
         self.name = name
@@ -16,25 +17,25 @@ class Stock:
     def sell(self, nshares):
         self.shares -= nshares
 
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls.types, row)]
+        return cls(*values)
+
 
 def read_portfolio(filename):
-    portfolio = []
     global headers
+    portfolio = None
     with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)  # Skip the header row
-        for row in rows:
-            name = row[0]
-            shares = int(row[1])
-            price = float(row[2])
-            s = Stock(name, shares, price)
-            portfolio.append(s)
+        portfolio = [Stock.from_row(row) for row in rows]
     return portfolio
 
 
 def print_portfolio(portfolio):
-    print(f"{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s}")
-    print(('-'*10 + ' ')*3)
+    # print(f"{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s}")
+    # print(('-'*10 + ' ')*3)
     for s in portfolio:
         print(f"{s.name:>10s} {s.shares:>10d} {s.price:>10.2f}")
 
