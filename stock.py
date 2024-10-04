b408df625c361a8f653a77080911d306a37a4fc2
Milco Numan <milco.numan@gmail.com>
2024-10-04 11:31:40 +0000

    3.4

--- stock.py
+++ stock.py
@@ -4,23 +4,48 @@ headers = []
 
 
 class Stock:
-    types = (str, int, float)
+    _types = (str, int, float)
+    __slots__ = ('name', '_shares', '_price')
 
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
 
-    def cost(self):
-        return self.shares * self.price
-
     def sell(self, nshares):
         self.shares -= nshares
 
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if isinstance(value, self._types[1]) and value >= 0:
+            self._shares = value
+        else:
+            raise TypeError('Expected positive non-negative integer')
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if isinstance(value, self._types[2]) and value >= 0:
+            self._price = value
+        else:
+            raise TypeError(
+                f'Expected positive non-negative {self._types[2].__name__}')
+
     @classmethod
     def from_row(cls, row):
-        values = [func(val) for func, val in zip(cls.types, row)]
+        values = [func(val) for func, val in zip(cls._types, row)]
         return cls(*values)
+
+    @property
+    def cost(self):
+        return self.shares * self.price
 
 
 def read_portfolio(filename):
